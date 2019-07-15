c4ae1670797dadb37f99822afb580a855ad291f8
maartencamps <52906716+maartencamps@users.noreply.github.com>
2019-07-15 08:20:24 +0200

    Create Bulk Create Teams.ps1

---
+++ Bulk Create Teams.ps1
@@ -0,0 +1,17 @@
+﻿# Bulk Create Teams
+# Maarten Camps
+# Camarate ICT
+
+[array]$teams = Import-Csv .\createteams.csv #Import CSV information in Array
+
+$prefix = "Team-" #Set defaiult prefix for each Team (Will return in Teams site on Sharepoint as well)
+
+foreach ($team in $teams) {
+
+    $t = $team.Name
+
+    $mailnickname = $prefix+$t #mailnickname is belangrijk om te vullen anders wordt deze random gegenereerd
+
+    New-Team -DisplayName $t -Description $t -MailNickName $mailnickname -AllowGuestCreateUpdateChannels $false -AllowGuestDeleteChannels $false -AllowDeleteChannels $false -AllowAddRemoveApps $false -AllowCreateUpdateRemoveTabs $false -AllowCreateUpdateChannels $false -AllowCreateUpdateRemoveConnectors $false -ShowInTeamsSearchAndSuggestions $true
+
+    }
