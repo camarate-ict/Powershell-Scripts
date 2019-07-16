1d61bc95788fa668736f65fc0e025c13c7a93dd5
maartencamps <52906716+maartencamps@users.noreply.github.com>
2019-07-16 07:32:11 +0200

    Create Bulk Update Team Names.ps1

---
+++ Bulk Update Team Names.ps1
@@ -0,0 +1,14 @@
+﻿# Bulk update Teams displaynames
+
+# Author Maarten Camps
+# Camarate ICT B.V.
+
+$teams = Get-Team
+
+foreach ($team in $teams) {
+
+    $displayname = $team.displayname.Replace("_"," ")
+
+    $groupid = $team.GroupId
+
+    Set-Team -GroupId $groupid -DisplayName $displayname}
